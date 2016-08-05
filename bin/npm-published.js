91dfae0a814ad6d5e2256ddf37185e8bbd0d867d
Branden Horiuchi <bhoriuchi@gmail.com>
2016-08-05 16:32:24 -0700

    adding dif functionality to check working node directory vs not working

--- bin/npm-published.js
+++ bin/npm-published.js
@@ -7,11 +7,12 @@ var _ = require('lodash')
 var util = require('util')
 var published = require('../lib')
 var opt = require('node-getopt').create([
-    ['s', 'source=ARG', 'package source (package.json or /node_modules)'],
-    ['f', 'from=ARG', 'filter out packages older than a specific date, optional'],
-    ['t', 'to=ARG', 'filter out packages newer than a specific date, optional'],
-    ['d', 'deps=ARG', 'dependencies to check, options are dev, prod, and all. all is default'],
-    ['', 'days', 'display how many days ago each module was published instead of the publish date']
+  ['s', 'source=ARG', 'package source (package.json or /node_modules)'],
+  ['f', 'from=ARG', 'filter out packages older than a specific date, optional'],
+  ['t', 'to=ARG', 'filter out packages newer than a specific date, optional'],
+  ['d', 'deps=ARG', 'dependencies to check, options are dev, prod, and all. all is default'],
+  ['', 'diff=ARG', ''],
+  ['', 'days', 'display how many days ago each module was published instead of the publish date']
 ]).bindHelp().parseSystem()
 
 //  constants
@@ -21,10 +22,15 @@ var deps = opt.options.deps
 var from = opt.options.from
 var to = opt.options.to
 var showDays = opt.options.days
+var diff = opt.options.diff
 
-var pub = published.query(source, from, to, { deps: deps, showDays: showDays })
-if (typeof pub.then === 'function') {
+if (source && diff) {
+  published.diff(source, diff)
+} else {
+  var pub = published.query(source, from, to, { deps: deps, showDays: showDays })
+  if (typeof pub.then === 'function') {
     pub.then(function (pkgs) {
-        console.log(JSON.stringify(pkgs, null, '  '))
+      console.log(JSON.stringify(pkgs, null, '  '))
     })
+  }
 }
